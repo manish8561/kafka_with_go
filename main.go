9cf165a1860db3ca409049badeb0fbe297304033
Manish Sharma <manish198646@yahoo.com>
2025-03-16 22:50:08 +0530

    make changes pics

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/twmb/franz-go/pkg/kadm"
 	"github.com/twmb/franz-go/pkg/kgo"
@@ -20,20 +21,34 @@ func getAdminClient() {
 	if err != nil {
 		panic(err)
 	}
-	// defer client.Close()
 
 	adminClient = kadm.NewClient(client)
 }
 
+func getKafkaSimpleClient() *kgo.Client {
+	client, err := kgo.NewClient(
+		kgo.SeedBrokers(brokers...),
+	)
+	if err != nil {
+		panic(err)
+	}
+	return client
+}
+
 func main() {
 	fmt.Println("starting.......")
-	getAdminClient()
 
+	//topic definition
 	topicName := "yt-kafka-lecture"
 
-	//configs
+	//init admin
+	getAdminClient()
+
+	// configs
 	// retention time
 	// properties sets
+
+	// ceration of topic
 	topicCreationResp, err := adminClient.CreateTopic(context.Background(), 5, -1, nil, topicName)
 
 	if err != nil {
@@ -41,4 +56,30 @@ func main() {
 	}
 
 	fmt.Println(topicCreationResp)
+
+	// simple producer client init
+	simpleClient := getKafkaSimpleClient()
+
+	ctx := context.Background()
+	// 1.) Producing a message
+	// All record production goes through Produce, and the callback can be used
+	// to allow for synchronous or asynchronous production.
+	var wg sync.WaitGroup
+	wg.Add(1)
+	// prepare record to produce over kafka
+	record := &kgo.Record{Topic: topicName, Value: []byte("Our second message to kafka!")}
+	// produce
+	simpleClient.Produce(ctx, record, func(_ *kgo.Record, err error) {
+		defer wg.Done()
+		if err != nil {
+			fmt.Printf("record had a produce error: %v\n", err)
+		}
+
+	})
+	wg.Wait()
+
+}
+
+func kafkaProducerClientClose(client *kgo.Client) {
+	client.Close()
 }
